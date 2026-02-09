63276d70b935d98ad3708b47b0bf4e8ad091da5c
Petar Juric <petarjuricbepo@yahoo.com>
2026-02-09 21:58:36 +0100

    this was a bit overboard

--- version-def.h
+++
@@ -1,8 +0,0 @@
-#ifndef VERSION_DEF_H
-#define VERSION_DEF_H
-
-#define GIT_VERSION "2.53.0.40.g3e0db84c88.dirty"
-#define GIT_BUILT_FROM_COMMIT "3e0db84c88c57e70ac8be8c196dfa92c5d656fbc"
-#define GIT_USER_AGENT "git/2.53.0.40.g3e0db84c88.dirty"
-
-#endif /* VERSION_DEF_H */
